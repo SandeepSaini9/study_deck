126dd9bf0cd8e7d91ca6836fe751ff9f6ea94faa
SandeepSaini9 <sandeepsaini74608@gmail.com>
2022-04-24 16:47:17 +0530

    aa

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:study_deck/signup.dart';
 
 class LoginPage extends StatefulWidget {
@@ -18,7 +20,8 @@ class _LoginPageState extends State<LoginPage> {
     double h = MediaQuery.of(context).size.height;
     return SafeArea(
       child: Scaffold(
-        backgroundColor: Colors.lightBlue,
+        resizeToAvoidBottomInset: false,
+        backgroundColor: Colors.blue,
         body: Container(
           height: h,
             width: w,
@@ -27,18 +30,11 @@ class _LoginPageState extends State<LoginPage> {
               Container(
                 width: w,
                 height: h*0.3,
-                // decoration: BoxDecoration(
-                //   image:  DecorationImage(
-                //     image: AssetImage(
-                //       "assets/images/Studydeck-logos/Studydeck-logos_white.png"
-                //     ),fit: BoxFit.fitWidth,
-                //   ),
-                // ),
                 child:const Image(image: AssetImage('assets/images/Studydeck-logos/Studydeck-logos_white.png'),fit: BoxFit.fitHeight, ),
 
               ),
               Container(
-                margin: const EdgeInsets.only(left: 20,right: 20),
+                margin: const EdgeInsets.only(left: 25,right: 25),
                 width: w,
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
@@ -51,43 +47,44 @@ class _LoginPageState extends State<LoginPage> {
                     //   ),
                     // ),
                     Text(
-                      "Sign Into Your Account",
+                      "Sign In To Your Account",
                       style: TextStyle(
                           fontSize: 20,
                           color:Colors.white,
                           fontWeight: FontWeight.bold
                       ),
                     ),
-                    SizedBox(height: 50,),
+                    SizedBox(height: 35,),
                     Container(
                       decoration: BoxDecoration(
                         color: Colors.white,
-                        borderRadius: BorderRadius.circular(20),
+                        borderRadius: BorderRadius.circular(10),
                         boxShadow: [
                           BoxShadow(
                             blurRadius: 10,
                             spreadRadius: 7,
                             offset: Offset(1,1),
-                            color: Colors.grey.withOpacity(0.5)
+                            color: Colors.grey.withOpacity(0.2)
                           )
                         ]
                       ),
                       child: TextField(
+                        keyboardType: TextInputType.phone,
                         decoration: InputDecoration(
-                            hintText: "Email",
-                            prefixIcon: Icon(Icons.email, color: Colors.deepOrangeAccent,),
+                            hintText: "Phone Number",
+                            prefixIcon: Icon(MdiIcons.phone, color: Colors.deepOrangeAccent,),
                           focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(20),
+                            borderRadius: BorderRadius.circular(10),
                             borderSide: BorderSide(
                               color: Colors.white,
-                              width: 1.0
+                              width: 1.5
                             )
                           ),
                           enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(20),
+                            borderRadius: BorderRadius.circular(10),
                             borderSide: BorderSide(
                               color: Colors.white,
-                              width: 1.0
+                              width: 1.5
                             )
                           ),
                           border: OutlineInputBorder(
@@ -100,36 +97,37 @@ class _LoginPageState extends State<LoginPage> {
                     Container(
                       decoration: BoxDecoration(
                           color: Colors.white,
-                          borderRadius: BorderRadius.circular(20),
+                          borderRadius: BorderRadius.circular(10),
                           boxShadow: [
                             BoxShadow(
                                 blurRadius: 10,
                                 spreadRadius: 7,
                                 offset: Offset(1,1),
-                                color: Colors.grey.withOpacity(0.5)
+                                color: Colors.grey.withOpacity(0.2)
                             )
                           ]
                       ),
                       child: TextField(
+                        obscureText: true,
                         decoration: InputDecoration(
                             hintText: "Password",
                             prefixIcon: Icon(Icons.password, color: Colors.deepOrangeAccent,),
                             focusedBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(20),
+                                borderRadius: BorderRadius.circular(10),
                                 borderSide: BorderSide(
                                     color: Colors.white,
                                     width: 1.0
                                 )
                             ),
                             enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(20),
+                                borderRadius: BorderRadius.circular(10),
                                 borderSide: BorderSide(
                                     color: Colors.white,
                                     width: 1.0
                                 )
                             ),
                             border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(20)
+                                borderRadius: BorderRadius.circular(10)
                             )
                         ),
                       ),
@@ -153,17 +151,18 @@ class _LoginPageState extends State<LoginPage> {
                   ],
                 ),
               ),
-              Container(
-                width: 200.0,
-                height: 50.0,
-                color: Colors.white70,
-                margin: EdgeInsets.all(20),
-                padding: EdgeInsets.all(9.0),
-                child: Text(
-                    "      Sign In", style: TextStyle(fontSize: 25),
-              )
-
-              ),
+               Container(
+                 width: 200,
+                 height: 50,
+                 child: TextButton(onPressed: (){}, child: Text('Login ', style: TextStyle(fontWeight: FontWeight.w700, fontSize: 18, color: Colors.black),), style: ButtonStyle(
+                     shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                         RoundedRectangleBorder(
+                             borderRadius: BorderRadius.circular(100.0),
+                             side: BorderSide(color: Colors.white)
+                         )
+                     )
+                 ), ),
+               ),
               SizedBox(height: w*0.2),
               RichText(text: TextSpan(
                 text: "Don\'t have an account?",
